2dbe26fad40130f1f236103b5cd48e6fe2f32696
id774 <idnanashi@gmail.com>
2013-08-27 08:45:12 +0900

    6 Change post url

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -13,19 +13,12 @@ describe "App" do
     @app ||= FluentdJsonReceiver
   end
 
-  def setup
-    @fluentd = Fluent::Logger::FluentLogger.open(nil,
-      host = '133.242.144.202',
-      port = 19999)
-    @fluentd.post('rspec.debug.forward', {"hoge" => "fuga"})
-  end
-
   it "should post succeed and return the ok" do
     @username = "username"
     @password = "password"
     json = {"hoge" => "fuga"}
     tag = "debug.forward"
-    response = RestClient.post("http://#{@username}:#{@password}@133.242.144.202/solve",
+    response = RestClient.post("http://#{@username}:#{@password}@133.242.144.202/post",
       {:tag => tag, :data => json},
       {:content_type => :json, :accept => :json})
     response.should be_empty
